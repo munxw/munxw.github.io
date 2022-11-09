760e5e505ad1a0e5e5db58518a8f4c82ea282afe
munxw_ <momunov.kr@gmail.com>
2022-11-09 14:28:20 +0600

    third commit

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-let hack_login = prompt("enter your login");
-let hack_pass = prompt("enter your password");
-console.log(hack_login);
-console.log(hack_pass);
+// let hack_login = prompt("enter your login");
+// let hack_pass = prompt("enter your password");
+// console.log(hack_login);
+// console.log(hack_pass);
+// console.log(document.referre);
+document.querySelector(".button_focuse").onclick = myclick;
+
+function myclick() {
+  //   console.log("work");
+  let a = document.querySelector(".input_focuse").value;
+  console.log(a);
+}
